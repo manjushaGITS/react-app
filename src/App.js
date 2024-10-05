1687688f6b3a98c5902d9b477537fb0924325f33
Manjusha Kotha <manjushakotha@Laxmans-iMac.verizon.net>
2024-10-05 10:42:04 -0400

    track: handle click

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    window.alert("i am clicked");
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +13,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p> Ading a new paragraph</p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <button onClick={handleClick}>Click me</button>
       </header>
     </div>
   );
